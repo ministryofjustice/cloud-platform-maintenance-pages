8e91ab2ad16906722e541309545554f54d9dccc1
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2024-09-03 15:26:57 +0100

    🔐 Configure Rack Protection Middleware (#104)

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ require "sinatra/content_for"
 
 set :environment, :production
 
+use Rack::Protection
+use Rack::Protection::FrameOptions
+use Rack::Protection::ContentSecurityPolicy,
+  default_src: "'self' 'unsafe-inline' data: https://region1.google-analytics.com https://www.googletagmanager.com"
+use Rack::Protection::XSSHeader
+use Rack::Protection::StrictTransport
+
 get "*" do
   erb request.host.to_sym
 end
